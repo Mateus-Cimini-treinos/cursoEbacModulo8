64b30fe924210e19bdced1abc9d38d96bf9baf21
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-10-30 14:32:14 -0300

    add botao de pesquisa e modal

--- Exercicio/script.js
+++ Exercicio/script.js
@@ -1,42 +1,57 @@
-console.log('carregou1');
+
+// MODAL
+
 const modal = document.querySelector('.modal');
 const openModalBtn = document.getElementById('btnAdicionar');
 const closeModalBtn = document.querySelector('.modal-close');
 const cancelBtn = document.querySelector('.modal-cancel');
-const saveBtn = document.querySelector('.modal-save'); // Assumindo que você tem um botão "Salvar" com essa classe
-const inputs = modal.querySelectorAll('input'); // Seleciona todos os inputs dentro do modal
+const saveBtn = document.querySelector('.modal-save');
+const inputs = modal.querySelectorAll('input');
 
-// Abre o modal
+// Abrir modal
 openModalBtn.addEventListener('click', () => {
     modal.classList.add('show');
-    console.log(`click`);
 });
 
-// Fecha o modal ao clicar no botão de fechar (X)
+
+//fechar modal
 closeModalBtn.addEventListener('click', () => {
     modal.classList.remove('show');
 });
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) modal.classList.remove('show');
+});
 
-// Limpa os campos de input e fecha o modal ao clicar no botão "Cancelar"
+
+// Limpar campos e fechar modal ao cancelar
 cancelBtn.addEventListener('click', () => {
-    inputs.forEach(input => {
-        input.value = ''; // Limpa cada campo de input
-    });
+    inputs.forEach(input => input.value = ''); 
     modal.classList.remove('show');
 });
 
-// Fecha o modal ao clicar fora do conteúdo
-modal.addEventListener('click', (e) => {
-    if (e.target === modal) {
-        modal.classList.remove('show');
-    }
-});
 
-// Fecha o modal ao clicar no botão "Salvar"
-// Adicione a lógica que você deseja para salvar aqui
+// botao de salvar contato (por enqianto limpa os campos e fecha o modal)
 saveBtn.addEventListener('click', () => {
-    // Lógica de salvamento (se necessário)
-    modal.classList.remove('show'); // Fecha o modal
+    inputs.forEach(input => input.value = ''); 
+    modal.classList.remove('show');
 });
 
-console.log('carregou');
+
+
+// BOTAO DE PESQUISA
+
+//Fazerndo o botao de pesquisa fucionar
+const pesquisa = document.querySelector(".pesquisa");
+const botao = document.querySelector(".botao");
+const pergunta = document.querySelector(".pergunta");
+
+botao.addEventListener("click" , () => {
+    pesquisa.classList.toggle("active");
+    pergunta.focus();
+});
+
+
+
+// TABELA
+
+const nome =
