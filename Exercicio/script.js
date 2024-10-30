2102b9a3605ca44002bf42e9b34ee953573a85d3
Mateus Cimini Treinos <mateusaraujo0123456789@gmail.com>
2024-10-30 17:19:19 -0300

    adicionando funçao de adicionar linha

--- Exercicio/script.js
+++ Exercicio/script.js
@@ -1,4 +1,17 @@
 
+// TABELA
+
+let nomeInput = '';
+let numeroInput = '';
+let linhas = [];
+let totalContatos = 0
+const form = document.getElementById('modal-form');
+
+
+//Botões de ação
+
+
+
 // MODAL
 
 const modal = document.querySelector('.modal');
@@ -8,45 +21,73 @@ const cancelBtn = document.querySelector('.modal-cancel');
 const saveBtn = document.querySelector('.modal-save');
 const inputs = modal.querySelectorAll('input');
 
-// Abrir modal
-openModalBtn.addEventListener('click', () => {
-    modal.classList.add('show');
-});
 
-
-//fechar modal
-closeModalBtn.addEventListener('click', () => {
-    modal.classList.remove('show');
-});
+// Abrir e Fechar Modal
+const toggleModal = (show) => modal.classList.toggle('show', show);
+openModalBtn.addEventListener('click', () => toggleModal(true));
+closeModalBtn.addEventListener('click', () => toggleModal(false));
 modal.addEventListener('click', (e) => {
-    if (e.target === modal) modal.classList.remove('show');
+    if (e.target === modal) toggleModal(false);
 });
 
 
-// Limpar campos e fechar modal ao cancelar
+// Limpar Campos e Fechar Modal ao Cancelar
+const clearInputs = () => inputs.forEach(input => input.value = '');
 cancelBtn.addEventListener('click', () => {
-    inputs.forEach(input => input.value = ''); 
-    modal.classList.remove('show');
+    clearInputs();
+    toggleModal(false);
 });
 
-
-// botao de salvar contato (por enqianto limpa os campos e fecha o modal)
+// botao de salvar contato (por enquanto limpa os campos e fecha o modal)
 saveBtn.addEventListener('click', () => {
+    nomeInput = document.getElementById('item-name').value;
+    numeroInput = document.getElementById('item-number').value;
 
-    const nomeValor = nome.value;  // Captura o valor do nome
-    const numeroValor = numero.value;  // Captura o valor do número
+
+    let linha = '<tr>';
+    linha += `<td>${nomeInput}</td>`;
+    linha += `<td>${numeroInput}</td>`;
+    linha += `
+    <td>
+        <button class="btn-edit"><i class="fa-solid fa-pen"></i></button>
+        <button class="btn-delete"><i class="fa-solid fa-trash"></i></button>
+    </td>`;
+    linha += `</tr>`;
+
     
-    inputs.forEach(input => input.value = ''); 
-    modal.classList.remove('show');
+
+
+    linhas += linha;
+    totalContatos += 1 
+    console.log(totalContatos);
     
-    console.log(`nome: ${nomeValor} numero: ${numeroValor}`);
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    atualizaTotalContatos();
+
+    clearInputs();
+    toggleModal(false);
 });
+
+function atualizaTotalContatos() {
+
+    let linhape = `<tr>`
+    linhape += `<td>Total</td>`
+    linhape += `<td colspan="5">${totalContatos} Contatos</td>`
+    linhape += `</tr>`
+
+
+    const rodapeTabela = document.querySelector('tfoot');
+    rodapeTabela.innerHTML = linhape
+}
 
 
 
 // BOTAO DE PESQUISA
 
-//Fazerndo o botao de pesquisa fucionar
+//Fazerndo o botao de pesquisa ter animaçao
 const pesquisa = document.querySelector(".pesquisa");
 const botao = document.querySelector(".botao");
 const pergunta = document.querySelector(".pergunta");
@@ -57,12 +98,28 @@ botao.addEventListener("click" , () => {
 });
 
 
+//fazendo ele realmente pesquisar
+const pesquisaInput = document.querySelector('.pergunta');
+pesquisaInput.addEventListener("input", () => {
+    const termoPesquisa = pesquisaInput.value.toLowerCase();
+    const linhasTabela = document.querySelectorAll('tbody tr');
 
-// TABELA
+    linhasTabela.forEach(linha => {
+        const nomeContato = linha.querySelector("td:first-child").textContent.toLocaleLowerCase();
+        const numeroContato = linha.querySelector("td:nth-child(2)").textContent.toLowerCase();
 
-const form = document.getElementById('modal-form');
-const nome = document.getElementById('item-name');
-const numero = document.getElementById('item-number');
+        if (nomeContato.includes(termoPesquisa) || numeroContato.includes(termoPesquisa)) {
+            linha.style.display = "";
+        } else {
+            linha.style.display = "none";
+
+        }
+    })
+})
+
+
+
+
 
 
 
